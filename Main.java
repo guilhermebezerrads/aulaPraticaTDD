b3df0a22b954623fab1ad95259c321aba7d9d690
Guilherme Bezerra <40393853+guilhermebezerrads@users.noreply.github.com>
2020-10-01 20:55:54 -0300

    (5)

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ class Main {
   public static void main(String[] args) {
     Testes teste = new Testes();
     teste.testMultiplication();
-    teste.testFrancMultiplication();
     teste.testEquality();
     teste.testCurrency();
   }
